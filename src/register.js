b589a5beab7663bcdf2199c38aca8ad624543182
Iván Sadofschi Costa <isadofschi@dm.uba.ar>
2020-07-09 23:16:41 -0300

    validaciones

--- src/register.js
+++ src/register.js
@@ -10,10 +10,33 @@ function register(data) {
 }
 
 function register_event(form) {
-    data = extract_data(form.closest("form"));
-    data.school_year = parseInt(data.school_year);
-    return register(data);
+    payload = extract_data(form.closest("form"));
+    payload.school_year = parseInt(payload.school_year);
+	validation_failures = validate_register_payload(payload);
+	if( validation_failures.length == 0 ){
+	    return register(data);
+	}else{
+		feedback_register_validation_fails(validation_failures);
+	}
 }
+
+function validate_register_payload(payload){
+	// to do: agregar todas las validaciones
+	var validation_failures = [];
+	if(payload["name"]==""){
+		validation_failures.push({ field:"Nombre", error: "No puede quedar vacío" });
+	}
+	if(payload["last_name"]==""){
+		validation_failures.push({ field:"Apellido", error: "No puede quedar vacío" });
+	}
+	return validation_failures;
+}
+
+function feedback_register_validation_fails(validation_failures){
+	validation_failures.forEach( x => notify("notification urgent", x.field, x.error) );
+}
+
+
 
 function province_selector() {
     ele = document.getElementById("province");
