771b95d69419592b7cfde1f2d150726be020e0df
cherny <cherny@brightsector.com>
2020-07-25 17:20:14 -0300

    Trying to fix the scroll for mobile

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
+function scrollToTop() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 function date_string(date, time) {
     var offset = new Date().getTimezoneOffset();
     if(offset >= 0) tz = "-"
