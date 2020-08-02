8e33d72ee98474d265437e7f4a0c968e1c6f3bee
cherny <cherny@brightsector.com>
2020-08-01 23:02:35 -0300

    Email en todos lados en vez de mail

--- src/password_forgotten.js
+++ src/password_forgotten.js
@@ -6,7 +6,7 @@ function recuperar_password(btn) {
 				"Revisá tu casilla de email! (si no llega en unos minutos, revisá la carpeta de \"Spam\" o \"No deseados\")");
 	}).catch(err => {
 		if (err.code == "email_not_found") {
-			notify("notification urgent", "El email es incorrecto", "Asegurate de haber escrito el mail con el cual te registraste");
+			notify("notification urgent", "El email es incorrecto", "Asegurate de haber escrito el email con el cual te registraste");
 		} else {
 			notify("notification urgent", "Ocurrió un error", "Volvé a intentar y si sigue persistiendo el error escribinos a omalbum.ok@gmail.com");
 		}
